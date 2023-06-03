7a9b932ce727361ed238682f8a016f87e5e52b0a
sameera sood <sameerasood@gmail.com>
2023-06-03 21:08:57 +0100

    set up database and seeding file for image files

---
+++ backend/database/seeds/art_space.sql
@@ -0,0 +1,18 @@
+DROP TABLE IF EXISTS image_files; 
+
+-- Table Definition
+CREATE TABLE image_files (
+    id SERIAL NOT NULL PRIMARY KEY,
+    date date,
+    filename text NOT NULL,
+    description text NOT NULL,
+    path text NOT NULL,
+);
+
+-- Seeding data
+
+-- INSERT INTO image_files ("name", "description") VALUES
+-- ('Raaya', 'Rock'),
+-- ('ABBA', 'Pop'),
+-- ('Taylor Swift', 'Pop'),
+-- ('Nina Simone', 'Pop');
